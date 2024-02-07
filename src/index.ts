20d8e60244c22f8af69bd0b4f6fe910e2aa94817
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-02-07 07:15:28 +0530

    backend completed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose  from "mongoose";
-
-
+import bodyParser from "body-parser";
+var cors=require('cors');
 const app=express();
 const port=3001;
-const uri = "mongodb+srv://Divyansh:Divyansh@cluster0.n8u9pu6.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Divyansh:Divyansh@cluster0.n8u9pu6.mongodb.net/test?retryWrites=true&w=majority";
 const connectionParams={
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -13,30 +13,19 @@ const connectionParams={
 
 
 
-async function run() {
-    mongoose.connect(uri).then(()=>
-    console.log("Mongo Atlas connected Successfully")
-    ).catch(
-        (err)=>console.log(err)
-    );
 
-    var result= await User.find().then((a)=>{
-        console.log(a)
-    }).catch((err)=>{
-        console.log(err);
-    })
-    // console.log(result);
-    mongoose.connection.close();
-    
-}
 
 const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    doctor:String,
+    patient_name:String,
+    patient_email:String,
+    doctor_name:String,
     id:Number
-})
+});
 
+const hashSchema=new mongoose.Schema({
+    link:String
+})
+const Hash=mongoose.models['hashes'] || mongoose.model("hashes",hashSchema);
 const User= mongoose.models['Users'] || mongoose.model("Users",userSchema);
 
 const user=new User({
@@ -46,12 +35,93 @@ const user=new User({
     id:1
 });
 
-run();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 
 
-user.save();
-app.get('/',(req,res)=>{
-    res.send("HEllo World");
+
+app.get('/data',async (req,res)=>{
+   await mongoose.connect(uri).then(()=>
+    console.log("Mongo Atlas connected Successfully")
+    ).catch(
+        (err)=>console.log(err)
+    );
+
+    var result= await User.find().then((a)=>{
+        res.send(a);
+    }).catch((err)=>{
+        console.log(err);
+    })
+    // console.log(result);
+    mongoose.connection.close();
+    
+    
+});
+
+
+app.get('/doctor',async (req,res)=>{
+   await mongoose.connect(uri).then(()=>
+    console.log("Mongo Atlas connected Successfully")
+    ).catch(
+        (err)=>console.log(err)
+    );
+
+    var result= await User.find().then((a)=>{
+        let doc=a.map((elem)=>{
+            return elem.doctor_name;
+        })
+
+        res.send(doc);
+    }).catch((err)=>{
+        console.log(err);
+    })
+    // console.log(result);
+    mongoose.connection.close();
+    
+    
+});
+
+
+app.post('/hash', async(req,res)=>{
+    await mongoose.connect(uri).then(()=>
+    console.log("Mongo Atlas connected Successfully")
+    ).catch(
+        (err)=>console.log(err)
+    );
+    const gethash=await req.body.hash;
+    console.log(gethash)
+    const storehash=new Hash({
+        link:gethash
+    });
+   await storehash.save().then(
+    ()=>{console.log("Hash saved successfully")}
+   );
+    res.json({"status":gethash})
+    mongoose.connection.close();
+});
+
+
+
+
+app.get('/allhash',async(req,res)=>{
+    await mongoose.connect(uri).then(()=>
+    console.log("Mongo Atlas connected Successfully")
+    ).catch(
+        (err)=>console.log(err)
+    );
+    var result=await Hash.find().then(
+        (a)=>{
+            let hashresponse=a.map((elem)=>{
+                return elem.link;
+            })
+            res.send(hashresponse);
+        }
+    ).catch((err)=>{console.log(err)});
+
+
+
+
+    mongoose.connection.close();
 })
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
